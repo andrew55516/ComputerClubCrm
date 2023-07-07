2a3d16b53de2e3c873cdb61eba519bc9765d1846
Andrey Aksenov <aksenovandrey4@gmail.com>
2023-07-07 22:07:32 +0300

    computerClub struct, event handlers

--- cmd/main.go
+++ cmd/main.go
@@ -6,19 +6,8 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"sort"
 	"strings"
 )
-
-type clientInfo struct {
-	table     int
-	satDownAt int
-}
-
-type tableInfo struct {
-	wasBusy int
-	revenue int
-}
 
 func main() {
 	if len(os.Args) != 2 {
@@ -33,17 +22,10 @@ func main() {
 	}
 
 	scanner := bufio.NewScanner(input)
-	builder := strings.Builder{}
 
-	var openTime, closeTime, tablesAmount, costPerHour, lastEventTime int
-
+	var lastEventTime, tablesAmount, openTime, closeTime, costPerHour int
 	var ok bool
-	var isTableBusy []bool
-	var tablesInfo []tableInfo
-
-	freeTables := 0
-	clients := make(map[string]clientInfo, 0)
-	waitList := make([]string, 0)
+	var club *internal.ComputerClub
 
 	i := 1
 	for scanner.Scan() {
@@ -55,10 +37,6 @@ func main() {
 				fmt.Printf("Error parsing tables amount: %s\n", row)
 				os.Exit(1)
 			}
-
-			freeTables = tablesAmount
-			isTableBusy = make([]bool, tablesAmount)
-			tablesInfo = make([]tableInfo, tablesAmount)
 
 		case 2:
 			parts := strings.Split(row, " ")
@@ -79,14 +57,15 @@ func main() {
 				os.Exit(1)
 			}
 
-			builder.WriteString(fmt.Sprintf("%s\n", internal.ParseMinutesToTime(openTime)))
-
 		case 3:
 			costPerHour, ok = internal.ParseInt(row)
 			if !ok {
 				fmt.Printf("Error parsing cost per hour: %s\n", row)
 				os.Exit(1)
 			}
+
+			club = internal.NewComputerClub(openTime, closeTime, tablesAmount, costPerHour)
+			club.Open()
 
 		default:
 			event, ok := internal.ParseEvent(row, tablesAmount)
@@ -96,115 +75,9 @@ func main() {
 			}
 
 			lastEventTime = event.Time
-			builder.WriteString(fmt.Sprintf("%s\n", row))
+			club.Recorder.WriteString(fmt.Sprintf("%s\n", row))
 
-			switch event.ID {
-			case 1:
-				if _, ok = clients[event.Name]; ok {
-					builder.WriteString(
-						fmt.Sprintf("%s 13 YouShallNotPass\n", internal.ParseMinutesToTime(event.Time)))
-					break
-				}
-				if event.Time < openTime {
-					builder.WriteString(
-						fmt.Sprintf("%s 13 NotOpenYet\n", internal.ParseMinutesToTime(event.Time)))
-					break
-				}
-				clients[event.Name] = clientInfo{}
-
-			case 2:
-				cl, ok := clients[event.Name]
-				if !ok {
-					builder.WriteString(
-						fmt.Sprintf("%s 13 ClientUnknown\n", internal.ParseMinutesToTime(event.Time)))
-					break
-				}
-
-				if isTableBusy[event.Table-1] {
-					builder.WriteString(
-						fmt.Sprintf("%s 13 PlaceIsBusy\n", internal.ParseMinutesToTime(event.Time)))
-					break
-				}
-
-				isTableBusy[event.Table-1] = true
-				freeTables--
-
-				if cl.table != 0 {
-					isTableBusy[cl.table-1] = false
-					freeTables++
-					tablesInfo[cl.table-1].wasBusy += event.Time - cl.satDownAt
-					tablesInfo[cl.table-1].revenue += revenue(event.Time-cl.satDownAt, costPerHour)
-				}
-
-				cl.table = event.Table
-				cl.satDownAt = event.Time
-				clients[event.Name] = cl
-
-			case 3:
-				cl, ok := clients[event.Name]
-
-				// I think the client must first come
-				if !ok {
-					builder.WriteString(
-						fmt.Sprintf("%s 13 ClientUnknown\n", internal.ParseMinutesToTime(event.Time)))
-					break
-				}
-
-				// If the client has already sat down he can't wait
-				if cl.table > 0 {
-					builder.WriteString(
-						fmt.Sprintf("%s 13 ClientHasBeenAlreadySatDown\n", internal.ParseMinutesToTime(event.Time)))
-					break
-				}
-
-				if freeTables > 0 {
-					builder.WriteString(
-						fmt.Sprintf("%s 13 ICanWaitNoLonger\n", internal.ParseMinutesToTime(event.Time)))
-					break
-				}
-
-				if len(waitList) == tablesAmount {
-					builder.WriteString(
-						fmt.Sprintf("%s 11 %s\n",
-							internal.ParseMinutesToTime(event.Time),
-							event.Name))
-					break
-				}
-
-				waitList = append(waitList, event.Name)
-
-			case 4:
-				cl, ok := clients[event.Name]
-				if !ok {
-					builder.WriteString(
-						fmt.Sprintf("%s 13 ClientUnknown\n", internal.ParseMinutesToTime(event.Time)))
-					break
-				}
-
-				if cl.table > 0 {
-					isTableBusy[cl.table-1] = false
-					freeTables++
-					tablesInfo[cl.table-1].wasBusy += event.Time - cl.satDownAt
-					tablesInfo[cl.table-1].revenue += revenue(event.Time-cl.satDownAt, costPerHour)
-
-					if len(waitList) > 0 {
-						next := waitList[0]
-						waitList = waitList[1:]
-
-						clients[next] = clientInfo{
-							table:     cl.table,
-							satDownAt: event.Time,
-						}
-
-						freeTables--
-						isTableBusy[cl.table-1] = true
-
-						builder.WriteString(
-							fmt.Sprintf("%s 12 %s\n", internal.ParseMinutesToTime(event.Time), next))
-					}
-				}
-				delete(clients, event.Name)
-			}
+			club.Handlers[event.ID](club, event)
 		}
 
 		i++
@@ -219,39 +92,5 @@ func main() {
 		os.Exit(1)
 	}
 
-	remainingClients := make([]string, 0, len(clients))
-	for k, _ := range clients {
-		remainingClients = append(remainingClients, k)
-	}
-
-	sort.Strings(remainingClients)
-
-	for _, k := range remainingClients {
-		cl := clients[k]
-		if cl.table != 0 {
-			tablesInfo[cl.table-1].wasBusy += closeTime - cl.satDownAt
-			tablesInfo[cl.table-1].revenue += revenue(closeTime-cl.satDownAt, costPerHour)
-		}
-		builder.WriteString(
-			fmt.Sprintf("%s 11 %s\n",
-				internal.ParseMinutesToTime(closeTime),
-				k))
-	}
-
-	builder.WriteString(fmt.Sprintf("%s\n", internal.ParseMinutesToTime(closeTime)))
-
-	for i, t := range tablesInfo {
-		builder.WriteString(
-			fmt.Sprintf("%d %d %s\n", i+1, t.revenue, internal.ParseMinutesToTime(t.wasBusy)))
-	}
-
-	fmt.Printf(builder.String())
-}
-
-func revenue(time, costPerHour int) int {
-	r := time / 60 * costPerHour
-	if time%60 > 0 {
-		r += costPerHour
-	}
-	return r
+	club.Close()
 }
---
+++ internal/computerClub.go
@@ -0,0 +1,208 @@
+package internal
+
+import (
+	"fmt"
+	"sort"
+	"strings"
+)
+
+type ComputerClub struct {
+	OpenTime     int
+	CloseTime    int
+	TablesAmount int
+	CostPerHour  int
+	FreeTables   int
+	Recorder     strings.Builder
+	WaitList     []string
+	IsTableBusy  []bool
+	TablesInfo   []tableInfo
+	Clients      map[string]clientInfo
+	Handlers     map[int]handlerEventFunc
+}
+
+type tableInfo struct {
+	wasBusy int
+	revenue int
+}
+
+type clientInfo struct {
+	table     int
+	satDownAt int
+}
+
+type handlerEventFunc func(*ComputerClub, Event)
+
+func (club *ComputerClub) Close() {
+	remainingClients := make([]string, 0, len(club.Clients))
+	for k, _ := range club.Clients {
+		remainingClients = append(remainingClients, k)
+	}
+
+	sort.Strings(remainingClients)
+
+	for _, k := range remainingClients {
+		cl := club.Clients[k]
+		if cl.table != 0 {
+			club.TablesInfo[cl.table-1].wasBusy += club.CloseTime - cl.satDownAt
+			club.TablesInfo[cl.table-1].revenue += revenue(club.CloseTime-cl.satDownAt, club.CostPerHour)
+		}
+		club.Recorder.WriteString(
+			fmt.Sprintf("%s 11 %s\n",
+				ParseMinutesToTime(club.CloseTime),
+				k))
+	}
+
+	club.Recorder.WriteString(fmt.Sprintf("%s\n", ParseMinutesToTime(club.CloseTime)))
+
+	for i, t := range club.TablesInfo {
+		club.Recorder.WriteString(
+			fmt.Sprintf("%d %d %s\n", i+1, t.revenue, ParseMinutesToTime(t.wasBusy)))
+	}
+
+	fmt.Printf(club.Recorder.String())
+}
+
+func (club *ComputerClub) Open() {
+	club.Recorder.WriteString(fmt.Sprintf("%s\n", ParseMinutesToTime(club.OpenTime)))
+}
+
+func NewComputerClub(openTime, closeTime, tablesAmount, costPerHour int) *ComputerClub {
+	club := &ComputerClub{
+		OpenTime:     openTime,
+		CloseTime:    closeTime,
+		TablesAmount: tablesAmount,
+		CostPerHour:  costPerHour,
+		FreeTables:   tablesAmount,
+		Recorder:     strings.Builder{},
+		WaitList:     make([]string, 0),
+		IsTableBusy:  make([]bool, tablesAmount),
+		TablesInfo:   make([]tableInfo, tablesAmount),
+		Clients:      make(map[string]clientInfo, 0),
+		Handlers:     make(map[int]handlerEventFunc, 0),
+	}
+
+	club.Handlers[1] = eventID1
+	club.Handlers[2] = eventID2
+	club.Handlers[3] = eventID3
+	club.Handlers[4] = eventID4
+
+	return club
+}
+
+func eventID1(club *ComputerClub, event Event) {
+	if _, ok := club.Clients[event.Name]; ok {
+		club.Recorder.WriteString(
+			fmt.Sprintf("%s 13 YouShallNotPass\n", ParseMinutesToTime(event.Time)))
+		return
+	}
+	if event.Time < club.OpenTime {
+		club.Recorder.WriteString(
+			fmt.Sprintf("%s 13 NotOpenYet\n", ParseMinutesToTime(event.Time)))
+		return
+	}
+	club.Clients[event.Name] = clientInfo{}
+}
+
+func eventID2(club *ComputerClub, event Event) {
+	cl, ok := club.Clients[event.Name]
+	if !ok {
+		club.Recorder.WriteString(
+			fmt.Sprintf("%s 13 ClientUnknown\n", ParseMinutesToTime(event.Time)))
+		return
+	}
+
+	if club.IsTableBusy[event.Table-1] {
+		club.Recorder.WriteString(
+			fmt.Sprintf("%s 13 PlaceIsBusy\n", ParseMinutesToTime(event.Time)))
+		return
+	}
+
+	club.IsTableBusy[event.Table-1] = true
+	club.FreeTables--
+
+	if cl.table != 0 {
+		club.IsTableBusy[cl.table-1] = false
+		club.FreeTables++
+		club.TablesInfo[cl.table-1].wasBusy += event.Time - cl.satDownAt
+		club.TablesInfo[cl.table-1].revenue += revenue(event.Time-cl.satDownAt, club.CostPerHour)
+	}
+
+	cl.table = event.Table
+	cl.satDownAt = event.Time
+	club.Clients[event.Name] = cl
+}
+
+func eventID3(club *ComputerClub, event Event) {
+	cl, ok := club.Clients[event.Name]
+
+	// I think the client must first come
+	if !ok {
+		club.Recorder.WriteString(
+			fmt.Sprintf("%s 13 ClientUnknown\n", ParseMinutesToTime(event.Time)))
+		return
+	}
+
+	// If the client has already sat down he can't wait
+	if cl.table > 0 {
+		club.Recorder.WriteString(
+			fmt.Sprintf("%s 13 ClientHasBeenAlreadySatDown\n", ParseMinutesToTime(event.Time)))
+		return
+	}
+
+	if club.FreeTables > 0 {
+		club.Recorder.WriteString(
+			fmt.Sprintf("%s 13 ICanWaitNoLonger\n", ParseMinutesToTime(event.Time)))
+		return
+	}
+
+	if len(club.WaitList) == club.TablesAmount {
+		club.Recorder.WriteString(
+			fmt.Sprintf("%s 11 %s\n",
+				ParseMinutesToTime(event.Time),
+				event.Name))
+		return
+	}
+
+	club.WaitList = append(club.WaitList, event.Name)
+}
+
+func eventID4(club *ComputerClub, event Event) {
+	cl, ok := club.Clients[event.Name]
+	if !ok {
+		club.Recorder.WriteString(
+			fmt.Sprintf("%s 13 ClientUnknown\n", ParseMinutesToTime(event.Time)))
+		return
+	}
+
+	if cl.table > 0 {
+		club.IsTableBusy[cl.table-1] = false
+		club.FreeTables++
+		club.TablesInfo[cl.table-1].wasBusy += event.Time - cl.satDownAt
+		club.TablesInfo[cl.table-1].revenue += revenue(event.Time-cl.satDownAt, club.CostPerHour)
+
+		if len(club.WaitList) > 0 {
+			next := club.WaitList[0]
+			club.WaitList = club.WaitList[1:]
+
+			club.Clients[next] = clientInfo{
+				table:     cl.table,
+				satDownAt: event.Time,
+			}
+
+			club.FreeTables--
+			club.IsTableBusy[cl.table-1] = true
+
+			club.Recorder.WriteString(
+				fmt.Sprintf("%s 12 %s\n", ParseMinutesToTime(event.Time), next))
+		}
+	}
+	delete(club.Clients, event.Name)
+}
+
+func revenue(time, costPerHour int) int {
+	r := time / 60 * costPerHour
+	if time%60 > 0 {
+		r += costPerHour
+	}
+	return r
+}
